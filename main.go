9d25c12916bc9ae13634b155248dd22ca1c458dd
riyan-eng <fbriyant11@gmail.com>
2023-01-08 21:01:25 +0700

    add ninja struct

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
+type Ninja struct {
+	Name   string
+	Weapon string
+}
+
 func main() {
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
